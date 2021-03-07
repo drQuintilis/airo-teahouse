7f8eb061020a232f6d5814b2fb04bee22e92eef2
Yulia <dockstrange42@gmail.com>
2021-03-07 15:35:40 +0000

    First commit. Add teahouse app.

---
+++ teahouse/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from teahouse.models import ResponseItem, MenuItem
+
+# Register your models here.
+
+admin.site.register(ResponseItem)
+admin.site.register(MenuItem)
---
+++ teahouse/models.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class MenuItem(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+    )
+    image = models.ImageField(
+        verbose_name='Картинка'
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class ResponseItem(models.Model):
+    name = models.CharField(
+        max_length=80,
+        verbose_name='Имя',
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+    )
+    posted_time = models.DateTimeField(
+        default=timezone.now,
+        verbose_name='Время',
+    )
