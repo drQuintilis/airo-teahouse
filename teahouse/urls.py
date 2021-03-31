9db3c9c90fd49722cc07967409143cdfd90c98ca
Yulia <dockstrange42@gmail.com>
2021-03-31 15:40:28 +0000

    Add site.

---
+++ teahouse/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from teahouse import views
+
+urlpatterns = [
+    path('', views.main_site, name='main_site'),
+    path('о-чайной/', views.about, name='about'),
+]
--- teahouse/views.py
+++ teahouse/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main_site(request):
+    return render(request, 'teahouse/main.html', {})
+
+def about(request):
+    return render(request, 'teahouse/about.html', {})
